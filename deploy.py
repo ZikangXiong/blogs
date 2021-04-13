70c8e85310a80c3d5bc26d4a6d076ad4ed753a00
ZikangXiong <zikangxiong@icloud.com>
2021-04-13 16:55:01 -0400

    2021-04-13 16:55:01

--- deploy.py
+++ deploy.py
@@ -1,15 +1,21 @@
 import os
 import datetime
+import time
+
 from generator.readme import generate_readme
 from generator.index import generate_index
 
 
 def deploy():
+    tic = time.time()
     os.system("git add .")
     now = datetime.datetime.now()
     t = now.strftime("%Y-%m-%d %H:%M:%S")
     os.system(f'git commit -m "{t}"')
     os.system("git push")
+    toc = time.time()
+
+    print(f"deployment time: {toc - tic} sec.")
 
 
 if __name__ == '__main__':
